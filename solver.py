2c6bf6a68d94765a22f94c233b3d42e66b4eca9c
jmkahn@hmc.edu <jkahn1618@DESKTOP-69RA3T1.localdomain>
2019-11-25 20:41:59 -0800

    cleaned up old code and print statements, added comments

--- solver.py
+++ solver.py
@@ -6,9 +6,7 @@ DOMAIN = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 NROWS = 9
 NCOLS = 9
 
-##Solve l33t, make it smallest failure possible
 ##Check that domain of everything in assignment is always 9 when we arrive to it
-
 
 class SudokuSolver:
     def __init__(self, sudokuGame):
@@ -48,7 +46,7 @@ class SudokuSolver:
         for all the variables (as a dictionary)'''
         # return self.recursiveBacktrack(self.assignment
         assignment = self.recursiveBacktrack()
-        return (assignment, self.nodesExpanded) 
+        return (assignment, self.nodesExpanded)
 
 
     def recursiveBacktrack(self):
@@ -56,77 +54,51 @@ class SudokuSolver:
         Returns null if there is no solution for the given assignments, returns the solution otherwise'''
         self.nodesExpanded += 1
         assignment = self.assignment
-        #if self.isComplete(assignment): # check if assignment is complete
         if self.isComplete():
             return assignment
         else:
-            # print("BEFORE")
-            # print(assignment)
-            # print(self.game.puzzle)
-            var = self.MRV()
-            # print("var: ", var)
-            oldAssign = copy.deepcopy(assignment)
-            # varAssign = oldAssign[var].copy()
-            # oldAssign = copy.deepcopy(assignment[var])
-            # options = oldAssign[var].copy() # to avoid skipping options
-          #  oldAssign = self.LCV(oldAssign) #TODO: Order values in old assignment of var
+            var = self.MRV() #choose a cell to assign using MRV heuristic
+            oldAssign = copy.deepcopy(assignment) #store a copy of current assignment
+
             for val in oldAssign[var]:
-                # print("Options for ", var, "are", varAssign)
-                if self.consistent(var, val):
-                    assignment[var] = [val]
+                if self.consistent(var, val): #if a value is consistent with constraints
+                    assignment[var] = [val] #assign the value to the cell
                     self.game.addToGame(var, val)
-                    # print("We're adding ", val, "to ", var)
-                    # print(self.game.puzzle)
-                    # print(assignment)
-                    result = self.recursiveBacktrack()
-                    if result != False:
+                    result = self.recursiveBacktrack() #recurse
+
+                    if result != False: #if the subtree finds a solution, return it
                         return result
-                    # print("found result for", var)
                     else:
-                        self.game.removeFromGame(var)
-                        # print("We're removing ", val, "from ", var)
-                        # print(self.game.puzzle)
-                        # varAssign.remove(val)
-                        # oldAssign[var] = varAssign.copy()
-                        self.assignment = copy.deepcopy(oldAssign) # TODO: test copy types
+                        self.game.removeFromGame(var) #otherwise un-assign the value and try again
+                        self.assignment = copy.deepcopy(oldAssign)
                         assignment = self.assignment
-                        # print(assignment)
         return False #FAIL
 
-    ### TODO: Optimize this
-    # def isComplete(self, assignment):
-    #     '''checks if every variable in assignment has only 1 value assigned'''
-    #     for key in assignment.keys():
-    #         if len(assignment[key]) > 1:
-    #             print("key is", key)
-    #             print(assignment[key])
-    #             return False
-    #     return True
-
     def isComplete(self):
+        '''checks if all of the cells in the puzzle have been filled (aka, assigned
+        a value)'''
         for row in range(NROWS):
             for col in range(NCOLS):
-                # print('current row', row, 'current col', col, 'value:', self.game.puzzle[row][col])
                 if self.game.puzzle[row][col] == 0:
                     return False
         return True
 
-    ### TODO: game object does not seem to be updated with the assignment
     def consistent(self, var, val):
-        ''' Checks that assignment of variable var does not violate CSP constraints '''
-        # print("In consistent: ", var, val)
-        row = self.game.getRow(var[0]).copy()
-        row[var[1]] = val
-        # print("row is ", row)
-        col = self.game.getCol(var[1]).copy()
+        ''' Checks that assignment of variable var does not violate Sukodu's constraints '''
+
+        row = self.game.getRow(var[0]).copy() #make a copy of the values in the current row
+        row[var[1]] = val #assign the new value to the row
+
+        col = self.game.getCol(var[1]).copy() #repeat with column
         col[var[0]] = val
-        # print("col is ", col)
-        box = self.game.getBox(var[0], var[1]).copy()
+
+        box = self.game.getBox(var[0], var[1]).copy() #repeat with box
         index = ((var[0]%3)+1) * ((var[1]%3)+1) -1
         box[index] = val
-        # print("box is ", box)
-        # print()
+
+        # return whether all the values are different
         return self.allDiff(row) and self.allDiff(col) and self.allDiff(box)
+
 
     ### TODO: Think about implementing highest degree variable tie breaker
     def MRV(self):
@@ -136,7 +108,6 @@ class SudokuSolver:
         minSoFar = ()
         minLen = 9
         for key in assignment.keys():
-            # print("IN MRV: ", key, assignment[key] )
             if len(assignment[key]) <= minLen and len(assignment[key]) > 1:
                 minSoFar = key
                 minLen = len(assignment[key])
